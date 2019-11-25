722f6d190d63f72fbc5bfa63cc97ab4a94958fab
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-11-25 11:11:49 +0530

    [CHORE] UI/UX Enhancements
    1. Dark theme enabled
    2. New Service for customize theme
    3. Dest input made editable
    4. Navbar styling tweaked
    5. Added ast-builder and ast-finder links in nav
    6. Fixed github and issues url

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,16 +3,19 @@ import { computed } from '@ember/object';
 import { buildAST } from 'ast-node-builder';
 import { dispatchNodes } from 'ast-node-finder';
 import { parse } from 'recast';
+import { inject as service } from '@ember/service';
 
 const _source = 'foo()';
 const _dest = 'foo.bar()';
 export default Controller.extend({
-  theme: 'solarized light',
+  customize: service(),
+  theme: computed.reads('customize.theme'),
   source: _source,
   dest: _dest,
   nodeOp: 'remove', 
   showDest: computed('nodeOp', function() {
-    return this.get('nodeOp') === 'replace' ? true : false;
+    let _show  = this.get('nodeOp') === 'replace' || this.get('nodeOp') === 'insert';
+    return  _show;
   }),
   nodeApi: computed('source', 'opQuery', function() {
     let ast = parse(this.get('source'));
@@ -30,7 +33,7 @@ export default function transformer(file, api) {
     return transformTemplate;
   }),
 
-  opQuery: computed('nodeOp', function() {
+  opQuery: computed('nodeOp', 'dest', function() {
     let str = '';
     switch(this.get('nodeOp')) {
       case 'remove':
---
+++ app/services/customize.js
@@ -0,0 +1,16 @@
+import Service from '@ember/service';
+import { computed } from '@ember/object';
+
+export default Service.extend({
+
+  darkMode: false,
+
+  theme: computed('darkMode', function() {
+    return this.get('darkMode') ? 'solarized dark' : 'solarized light';
+  }),
+
+  toggleDarkMode() {
+    let dm = this.get('darkMode');
+    this.set('darkMode', !dm);
+  }
+});
