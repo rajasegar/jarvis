f3df5957af33e85f4f149ceb5fa2377a7dfd91da
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-12-09 12:35:09 +0530

    [CHORE] Show node builder and finder versions in footer
    1. Bump builder and finder versions

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,19 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { computed } from "@ember/object";
+import ENV from "jarvis/config/environment";
 
 export default Controller.extend({
 customize: service(),
+  emberVersion: computed(function() {
+    return ENV.pkg.devDependencies["ember-source"];
+  }),
+  nodeBuilderVersion: computed(function() {
+    return ENV.pkg.dependencies["ast-node-builder"];
+  }),
+  nodeFinderVersion: computed(function() {
+    return ENV.pkg.dependencies["ast-node-finder"];
+  }),
   actions: {
     toggleDarkMode() {
       this.customize.toggleDarkMode();
