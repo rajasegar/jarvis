34d5894afe38e7283e3e14bc2e841dff1243e849
Rajasegar <rajasegar.c@gmail.com>
2023-01-21 12:51:11 +0530

    chore: ux improvements
    1. add panels for all the editors with description

--- app/modifiers/code-mirror.js
+++ app/modifiers/code-mirror.js
@@ -4,16 +4,65 @@ import { EditorView, basicSetup } from "codemirror";
 import { StateField, EditorState } from "@codemirror/state";
 import { javascript } from "@codemirror/lang-javascript";
 import { json } from "@codemirror/lang-json";
+import { autocompletion } from "@codemirror/autocomplete";
+import { showPanel } from "@codemirror/view";
+
+function myCompletions(context) {
+  let word = context.matchBefore(/\w*/);
+  if (word.from == word.to && !context.explicit) return null;
+  return {
+    from: word.from,
+    options: [
+      { label: "match", type: "keyword" },
+      { label: "hello", type: "variable", info: "(World)" },
+      { label: "magic", type: "text", apply: "⠁⭒*.✩.*⭒⠁", detail: "macro" },
+      {
+        label: "j.callExpression",
+        type: "function",
+        info: "j.callExpression(expression)",
+        apply: "j.callExpression()",
+      },
+      {
+        label: ".findFunctionDeclarations",
+        type: "function",
+        info: "j.findFunctionDeclarations(name?: string)",
+        apply: "findFunctionDeclarations('foo')",
+      },
+    ],
+  };
+}
 
 export default modifier(function codeMirror(
   element,
   [], // eslint-disable-line
-  { content, onUpdate, mode }
+  { content, onUpdate, mode, helperText }
 ) {
   if (!element) {
     throw new Error("CodeMirror modifier has no element");
   }
 
+  function createHelpPanel() {
+    let dom = document.createElement("div");
+    dom.textContent = helperText;
+    dom.className = "cm-help-panel";
+    return { top: true, dom };
+  }
+
+  const helpTheme = EditorView.baseTheme({
+    ".cm-help-panel": {
+      padding: "5px 10px",
+      backgroundColor: "#fffa8f",
+      fontFamily: "monospace",
+    },
+  });
+
+  const helpPanelState = StateField.define({
+    create: () => true,
+    provide: (f) => showPanel.from(f, (on) => (on ? createHelpPanel : null)),
+  });
+  function helpPanel() {
+    return [helpPanelState, helpTheme];
+  }
   const listenChangesExtension = StateField.define({
     create: () => null,
     update: async (value, transaction) => {
@@ -25,7 +74,15 @@ export default modifier(function codeMirror(
     },
   });
 
-  const extensions = [basicSetup, listenChangesExtension];
+  const extensions = [
+    basicSetup,
+    listenChangesExtension,
+    autocompletion({ override: [myCompletions] }),
+  ];
+
+  if (helperText) {
+    extensions.push(helpPanel());
+  }
   if (mode === "js") {
     extensions.push(javascript());
   }
